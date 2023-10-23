52081992239e6e2e4bc99f6261aea947676e6cf8
Damien COLLET LAMOUREUX <damien.collet@epitech.eu>
2023-10-23 16:50:38 +0200

    done

---
+++ include/my.h
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2023
+** my
+** File description:
+** got all prototype of my lib
+*/
+
+#ifndef MY_H
+    #define MY_H
+int my_put_nbr(int nb);
+void my_putchar(char c);
+int my_putstr(char const *str);
+#endif /* MY_H */
