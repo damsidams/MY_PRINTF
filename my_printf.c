50c99911e9481bde6c1d5b92e91bf167c9d07408
Damien COLLET LAMOUREUX <damien.collet@epitech.eu>
2023-10-24 15:00:04 +0200

    my_printf

---
+++ my_printf.c
@@ -0,0 +1,52 @@
+/*
+** EPITECH PROJECT, 2023
+** mini_printf
+** File description:
+** reproduce basic printf behavior
+*/
+
+#include <stdarg.h>
+#include "include/my.h"
+
+void cases(char c, va_list list, int *counter)
+{
+    switch (c){
+    case 'd':
+    case 'i':
+        (*counter) = (*counter)	+ my_put_nbr(va_arg(list, int));
+        break;
+    case 's':
+        (*counter) = (*counter)	+ my_putstr(va_arg(list, char *));
+        break;
+    case 'c':
+        my_putchar(va_arg(list, int));
+        (*counter) = (*counter)	+ 1;
+        break;
+    case '%':
+        (*counter) = (*counter) + 1;
+        my_putchar('%');
+    break;
+    default:
+        my_putchar('%');
+        my_putchar(c);
+    }
+}
+
+int mini_printf(const char *format, ...)
+{
+    va_list list;
+    int counter;
+
+    va_start(list, format);
+    while (*format){
+        if (*format == '%'){
+            format++;
+            cases(*format, list, &counter);
+        } else {
+            counter++;
+            my_putchar(*format);
+        }
+        format++;
+    }
+    return counter;
+}
