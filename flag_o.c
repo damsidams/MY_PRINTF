2d90726a7ef148403d801481537b1a57457a0692
Damien COLLET LAMOUREUX <damien.collet@epitech.eu>
2023-10-24 12:15:07 +0200

    flag o

---
+++ flag_o.c
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2023
+** flag_o
+** File description:
+** case of flag o
+*/
+
+int flag_o(int i)
+{
+    int rest = 0;
+    int quot = 0;
+
+    if (i <= 0)
+        return 0;
+    rest = i % 8;
+    quot = (i - rest) / 8;
+    my_put_nbr(rest);
+    return 1 + flag_o(quot);    
+}
