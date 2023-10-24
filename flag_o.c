f793e9d4a489a44c7a18b64d1af0233db1ed2c13
Damien COLLET LAMOUREUX <damien.collet@epitech.eu>
2023-10-24 20:21:57 +0200

    order of display fixed

--- flag_o.c
+++ flag_o.c
@@ -11,11 +11,13 @@ int flag_o(int i)
 {
     int rest = 0;
     int quot = 0;
+    int nbr = 0;
 
     if (i <= 0)
         return 0;
     rest = i % 8;
     quot = (i - rest) / 8;
+    nbr += 1 + flag_o(quot);
     my_put_nbr(rest);
-    return 1 + flag_o(quot);
+    return nbr;
 }
