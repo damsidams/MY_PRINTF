77daa256ffb79f2d109b8b72afeffa41c1b42556
Damien COLLET LAMOUREUX <damien.collet@epitech.eu>
2023-10-25 23:46:56 +0200

    my_printf

--- my_printf.c
+++ my_printf.c
@@ -8,34 +8,77 @@
 #include <stdarg.h>
 #include "include/my.h"
 
-void print_int(va_list list, int *counter)
+static void print_int(va_list list, int *counter)
 {
     (*counter) = (*counter) + my_put_nbr(va_arg(list, int));
 }
 
-void print_string(va_list list, int *counter)
+static void print_string(va_list list, int *counter)
 {
     (*counter) = (*counter) + my_putstr(va_arg(list, char *));
 }
 
-void print_char(va_list list, int *counter)
+static void print_char(va_list list, int *counter)
 {
     my_putchar(va_arg(list, int));
     (*counter) = (*counter) + 1;
 }
 
-void print_percent(va_list list, int *counter)
+static void print_percent(va_list list, int *counter)
 {
     (*counter) = (*counter) + 1;                                                                     
     my_putchar('%');
+}
+
+static void print_octal(va_list list, int *counter)
+{
+    (*counter) = (*counter) + flag_o(va_arg(list, unsigned int));
+}
+
+static void print_unsigned(va_list list, int *counter)
+{
+    (*counter) = (*counter) + flag_u(va_arg(list, unsigned int));
+}
+
+static void print_hex(va_list list, int *counter)
+{
+    (*counter) = (*counter) + flag_x(va_arg(list, unsigned int));
+}
+
+static void print_hexx(va_list list, int *counter)
+{
+    (*counter) = (*counter) + flag_xx(va_arg(list, unsigned int));
+}
+
+static void print_float(va_list list, int *counter)
+{
+    (*counter) = (*counter) + flag_f(va_arg(list, double));
+}
+
+static void print_ffloat(va_list list, int *counter)
+{
+    (*counter) = (*counter) + flag_ff(va_arg(list, double));
+}
+
+static void print_floatP(va_list list, int *counter)
+{
+    (*counter) = (*counter) + flag_e(va_arg(list, double));
+}
+
+static void print_ffloatP(va_list list, int *counter)
+{
+    (*counter) = (*counter) + flag_ee(va_arg(list, double));
 }
 
 void cases(char c, va_list list, int *counter)
 {
     void (*functions[])(va_list list, int *counter) =
         {print_int, print_string,
-        print_char, print_percent};
-    char *base = "dsc%";
+         print_char, print_percent, print_int,
+         print_octal, print_unsigned, print_hex,
+         print_hexx};
+    char *base = "dsc%iouxXfFeE";
+
     for (int i = 0; base[i] != '\0'; i++){
         if (base[i] == c){
             functions[i](list, counter);
