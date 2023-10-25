2fe124c4b9dc3df3fb26d12f4d6b4a051edf24c4
Enzo Fokwen <enzo.fokwen-rocher@epitech.eu>
2023-10-25 09:45:46 +0200

    converted the input of flag f in a double (was a float before) so can now have more precision

--- flag_funcs.c
+++ flag_funcs.c
@@ -12,7 +12,8 @@ int flag_f(double nb)
 {
     int nb_of_char = 0;
     int i = 0;
-
+    int new_nb = 0;
+    //printf("Here is i: %d, and nb: %f\n", i, nb);
     if (nb < 0) {
         my_putchar('-');
         nb *= -1;
@@ -22,14 +23,50 @@ int flag_f(double nb)
     if (i == nb)
         return my_put_nbr(nb);
     nb_of_char = my_put_nbr(i - 1);
+    //printf("Here is i: %d, and nb: %f\n", i, nb);
     nb = nb - (i - 1);
-    //printf("This is the dec nbr: %f\n", nb);
-    nb = nb * 1000000;
+    //printf("this is the nb: %f\n", nb);
+    i = 0;
     my_putchar('.');
-    nb_of_char += my_put_nbr(nb) + 1;
+    while (i < 6) {
+        nb = nb * 10;
+        //printf("This is nb lopp: %f\n", nb);
+        for (int j = 0; j < nb; j++)
+            new_nb = j;
+        nb_of_char += my_put_nbr(new_nb);
+        nb -= new_nb;
+        i++;
+    }
+    //nb_of_char += my_put_nbr(nb) + 1;
     return nb_of_char;
 }
+/*
+int flag_f(float nb)
+{
+    int nb_of_char = 0;
+    int i = 0;
+    int res;
 
+    if (nb < 0) {
+        my_putchar('-');
+        nb *= -1;
+    }
+    res = nb / 1;
+    printf("This is res: %f\n", res);
+    //if ((nb - res) == 0)
+    //    return my_put_nbr(nb);
+    nb_of_char = my_put_nbr(res);
+    nb -= res;
+    my_putchar('.');
+    while (i < 6) {
+        nb = nb * 10;
+        res = nb / 1;
+        nb_of_char += my_put_nbr(res);
+        i ++;
+    }
+    return nb_of_char + 1;
+}
+*/
 int flag_s(char *str)
 {
     return my_putstr(str);
@@ -37,8 +74,8 @@ int flag_s(char *str)
 
 static int flag_e_printer(float nb, char pos, int base)
 {
-    int nb_of_char = 0;
-    
+    int nb_of_char = 0;    
+
     nb_of_char = flag_f(nb);
     my_putchar('e');
     my_putchar(pos);
@@ -55,12 +92,12 @@ int flag_e(float nb)
 
     while (nb > 10) {
         nb /= 10;
-	base ++;
+        base ++;
     }
     while (nb < 1) {
-	pos = '-';
+        pos = '-';
         nb *= 10;
         base ++;
-    }//check the return value
+    }
     return flag_e_printer(nb, pos, base) + 4;
 }
