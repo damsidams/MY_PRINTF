97d6f0ae836e77dd5375152621576d2f0174455c
Enzo Fokwen <enzo.fokwen-rocher@epitech.eu>
2023-10-26 16:12:58 +0200

    changed the way of counting characters as the return wasn't good

--- my_put_nbr.c
+++ my_put_nbr.c
@@ -7,16 +7,17 @@
 
 #include "include/my.h"
 
-static int print_nb(int nb, int nb_char_print)
+static void print_nb(int nb, int *nb_char_print)
 {
     char res;
 
     if (nb / 10 > 0)
-        nb_char_print += print_nb(nb / 10, nb_char_print);
+        print_nb(nb / 10, nb_char_print);
     res = (nb % 10) + '0';
     my_putchar(res);
-    nb_char_print ++;
-    return nb_char_print;
+    //printf("This is the nb at start of res: %d\n", nb_char_print);
+    *nb_char_print += 1;
+    //printf("This is the nb at the end of res: %d\n", nb_char_print);
 }
 
 int my_put_nbr(int nb)
@@ -28,6 +29,6 @@ int my_put_nbr(int nb)
         nb = nb * -1;
         nb_char_print ++;
     }
-    nb_char_print = print_nb(nb, nb_char_print);
+    print_nb(nb, &nb_char_print);
     return nb_char_print;
 }
