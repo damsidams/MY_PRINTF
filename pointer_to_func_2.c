75016dd24c035bb6ae51fbba7f012d3f0f283612
Enzo Fokwen <enzo.fokwen-rocher@epitech.eu>
2023-10-31 19:51:26 +0100

    [STYLE] pointer_to_func_2.c -> fixed the coding style

--- pointer_to_func_2.c
+++ pointer_to_func_2.c
@@ -8,10 +8,8 @@
 #include <stdarg.h>
 #include "include/my.h"
 
-//replace nb by va_list and va_arg
 int print_float(va_list list, char char_flag, int bp, int ap)
 {
-    printf("In the flag f\n");
     int nb_of_char = 0;
     double arg = va_arg(list, double);
     char flag = 'f';
@@ -20,7 +18,7 @@ int print_float(va_list list, char char_flag, int bp, int ap)
         flag_zero, flag_plus, flag_blank, flag_hashtag, flag_minus_int,
         flag_minus_double
     };
-    
+
     if (choose_cflag(char_flag, flag) >= 0)
         nb_of_char += f_char[choose_cflag(char_flag, flag)](arg, flag, bp);
     if (char_flag == '-')
@@ -38,10 +36,11 @@ int print_ffloat(va_list list, char char_flag, int bp, int ap)
         flag_zero, flag_plus, flag_blank, flag_hashtag, flag_minus_int,
         flag_minus_double
     };
+
     if (choose_cflag(char_flag, flag) >= 0)
         nb_of_char += f_char[choose_cflag(char_flag, flag)](arg, flag, bp);
     if (char_flag == '-')
-	return nb_of_char;
+        return nb_of_char;
     return nb_of_char + flag_ff(arg);
 }
 
@@ -55,6 +54,7 @@ int print_float_e(va_list list, char char_flag, int bp, int ap)
         flag_zero, flag_plus, flag_blank, flag_hashtag, flag_minus_int,
         flag_minus_double
     };
+
     if (choose_cflag(char_flag, flag) >= 0)
         nb_of_char += f_char[choose_cflag(char_flag, flag)](arg, flag, bp);
     if (char_flag == '-')
@@ -72,6 +72,7 @@ int print_ffloat_ee(va_list list, char char_flag, int bp, int ap)
         flag_zero, flag_plus, flag_blank, flag_hashtag, flag_minus_int,
         flag_minus_double
     };
+
     if (choose_cflag(char_flag, flag) >= 0)
         nb_of_char += f_char[choose_cflag(char_flag, flag)](arg, flag, bp);
     if (char_flag == '-')
@@ -82,7 +83,7 @@ int print_ffloat_ee(va_list list, char char_flag, int bp, int ap)
 //change arg to va_list, va_arg
 int print_int(va_list list, char char_flag, int bp, int ap)
 {
-    int nb_of_char = 0;
+    int nchar = 0;
     int arg = va_arg(list, int);
     char flag = 'd';
     int (*f_char[])(double, char, int) =
@@ -90,10 +91,11 @@ int print_int(va_list list, char char_flag, int bp, int ap)
         flag_zero, flag_plus, flag_blank, flag_hashtag, flag_minus_int,
         flag_minus_double
     };
+
     if (choose_cflag(char_flag, flag) >= 0) {
         if (char_flag == '-')
             return f_char[choose_cflag(char_flag, flag)](arg, flag, bp);
-	nb_of_char += f_char[choose_cflag(char_flag, flag)]((double)arg, flag, bp);
+        nchar += f_char[choose_cflag(char_flag, flag)]((double)arg, flag, bp);
     }
-    return nb_of_char + my_put_nbr(arg);
+    return nchar + my_put_nbr(arg);
 }
