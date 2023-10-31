5213b5ce669f71397201e9a95775ab30aa6ca7d9
Damien COLLET LAMOUREUX <damien.collet@epitech.eu>
2023-10-31 20:07:14 +0100

    semi stable

--- my_printf.c
+++ my_printf.c
@@ -9,16 +9,9 @@
 #include "include/my.h"
 #include "include/p_function.h"
 
-static print_noflag(char c)
-{
-    my_putchar('%');
-    my_putchar(c);
-}
-
 static int cases(char c, va_list list, char identifier, int *precisions)
 {
-    int (*functions[])
-        (va_list list, char identifier, int precisions1, int precisions2) =
+    int (*functions[])(va_list list, char identifier, int precisions1, int precisions2) =
         {
             print_int, print_string,
             print_char, print_percent, print_int,
@@ -28,22 +21,19 @@ static int cases(char c, va_list list, char identifier, int *precisions)
             print_theggflag, print_n, print_pointer,
             print_a, print_aa};
     char *base = "dsc%iouxXfFeEgGnpaA";
-
     for (int i = 0; base[i] != '\0'; i++){
         if (base[i] == c){
-            return functions[i](list, identifier,
-                                precisions[0], precisions[1]);
+            return functions[i](list, identifier, precisions[0], precisions[1]);
         }
     }
-    print_noflag(c);
+    my_putchar('%');
+    my_putchar(c);
     return 2;
 }
 
-static int precisions
-(const char *c, va_list list, int *counter, char identifier)
+static int precisions(const char *c, va_list list, int *counter, char identifier)
 {
     int res[2];
-
     while (my_char_isnum(*c) == 1){
         res[0] = res[0] * 10 + (*c - '0');
         c++;
@@ -61,34 +51,34 @@ static int precisions
 static int format_identifiers(const char *c, va_list list, int *counter)
 {
     char *base = "-0+ #";
-
     for (int i = 0; base[i] != '\0'; i++){
         if (base[i] == *c){
             c++;
-            precisions(c, list, counter, base[i]);
-            return 0;
+            return 1 + precisions(c, list, counter, base[i]);
         }
     }
     precisions(c, list, counter, 0);
-    return 1;
+    return 0;
 }
 
 int my_printf(const char *format, ...)
 {
     va_list list;
     int counter;
-    char next_letter = *(format) + 1;
+    char next_letter = *(format + 1);
+    int flag_count = 1;
 
     va_start(list, format);
     while (*format){
-        next_letter = *(format) + 1;
+        next_letter = (*format) + 1;
         if (*format == '%' && next_letter != '\0'){
-            format_identifiers(format, list, &counter);
+            format++;
+            flag_count = format_identifiers(format, list, &counter);
         } else {
             counter += my_putchar(*format);
         }
-//        if (next_letter != '\0')
-        format++;
+        if (format != '\0')
+            format += flag_count;
     }
     va_end(list);
     return counter;
